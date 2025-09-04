156f1c8812afe6e7a3d3c82e024bad064447432c
Deepika . <deepikadarisipudi2929@gmail.com>
2025-09-04 22:39:18 +0530

    initial code push'

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const App = () => {
+  // const h1Element = React.createElement(
+  // "h1" ,
+  // { className : "Header" },
+  // "Hello, React!"
+  // );
+  // const pElement = React.createElement(
+  // "p" ,
+  // { className : "Paragraph" },
+  // "This is paragraph"
+  // );
+
+  // Define the handleClick function
+  const handleClick = () => {
+    alert("Button clicked!");
+    console.log("Button was clicked");
+    
+  };
+
+  return (
+    <div>
+      <h1 className="Header">Hello, Deepu's React!</h1>
+      <button onClick={handleClick}> Click Me </button>
+      <p className="Paragraph">This is paragraph</p>
+    </div>
+  );
+};
+
+export default App;
